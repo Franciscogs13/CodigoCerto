1ea34fb44d3915581a9d68b5d7c9e9f28c1b2097
Franciscogs13 <franciscogomessilva953@gmail.com>
2024-07-02 19:39:06 -0300

    comunidade codigo certo coders

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#text", {
+  strings:["Desenvolvedor Front-End"],
+  typeSpeed:100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+})
+
+document.querySelector(anchor.getAttribute('href').slice(1)).scrollIntoView({
+  behavior: 'mooth'
+});
